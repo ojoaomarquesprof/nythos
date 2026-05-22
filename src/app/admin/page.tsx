e67865ce9341b576fa04f8fbea4a27b409d24d83
João Marques <ojoaomarques.jj@gmail.com>
2026-05-22 13:33:11 -0300

    meu outro deploy

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -309,7 +309,7 @@ export default function AdminPage() {
                             setModalOpen(true);
                           }}
                         >
-                          Gerenciar
+                          Gerenciar.
                         </Button>
                       </td>
                     </tr>
